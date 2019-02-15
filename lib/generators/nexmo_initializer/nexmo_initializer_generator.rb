a9803020571aa07032528ca4c3b491d843d4196f
benhayehudi <bengreenberg@gmail.com>
2019-02-15 11:03:58 +0200

    fixed whitespace issue in initializer output

--- lib/generators/nexmo_initializer/nexmo_initializer_generator.rb
+++ lib/generators/nexmo_initializer/nexmo_initializer_generator.rb
@@ -5,14 +5,15 @@ class NexmoInitializerGenerator < Rails::Generators::Base
     desc "This generator creates a Nexmo initializer file at config/initializers and makes a configured Nexmo client globally available"
 
     def create_nexmo_initializer
-      initializer "nexmo.rb" do 
-        "::Nexmo = Nexmo::Client.new do |config|\n
-          config.api_key = ENV['NEXMO_API_KEY'],\n
-          config.api_secret = ENV['NEXMO_API_SECRET'],\n
-          config.api_signature = ENV['NEXMO_API_SIGNATURE'],\n
-          config.application_id = ENV['NEXMO_APPLICATION_ID'],\n
-          config.private_key = ENV['NEXMO_PRIVATE_KEY']\n
-        end"
+      initializer "nexmo.rb" do <<~HEREDOC 
+        ::Nexmo = Nexmo::Client.new do |config|
+          config.api_key = ENV['NEXMO_API_KEY'],
+          config.api_secret = ENV['NEXMO_API_SECRET'],
+          config.api_signature = ENV['NEXMO_API_SIGNATURE'],
+          config.application_id = ENV['NEXMO_APPLICATION_ID'],
+          config.private_key = ENV['NEXMO_PRIVATE_KEY']
+        end
+        HEREDOC
       end
     end
 end
