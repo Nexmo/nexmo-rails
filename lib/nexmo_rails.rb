26aa7082b30bbfbd690aa0ead4edac7a8f26cb9c
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2019-04-23 10:32:58 +0200

    Delegate API methods to client.

--- lib/nexmo_rails.rb
+++ lib/nexmo_rails.rb
@@ -1,8 +1,15 @@
 require 'nexmo'
+require 'forwardable'
 
 module Nexmo
   class << self
+    extend Forwardable
+
     attr_accessor :client
+
+    def_delegators :@client, :sms, :tfa, :calls, :verify,
+                   :number_insight, :applications, :numbers,
+                   :secrets, :redact, :signature
 
     def setup
       self.client = ::Nexmo::Client.new do |config|
