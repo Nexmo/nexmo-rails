225240e671d07dafce7f1139a9613f9b8db4933b
benhayehudi <bengreenberg@gmail.com>
2019-05-10 12:07:54 +0300

    add conversation apis to def_delegators

--- lib/nexmo_rails.rb
+++ lib/nexmo_rails.rb
@@ -9,7 +9,9 @@ module Nexmo
 
     def_delegators :@client, :sms, :tfa, :calls, :verify,
                    :number_insight, :applications, :numbers,
-                   :secrets, :redact, :signature
+                   :secrets, :redact, :signature, :conversations,
+                   :conversation_users, :conversation_members,
+                   :conversation_legs, :conversation_events
 
     def setup
       self.client = ::Nexmo::Client.new do |config|
