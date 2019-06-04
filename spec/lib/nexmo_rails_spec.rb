59fca5d6f151a9dcc7c7c46a3a762b327318a570
benhayehudi <bengreenberg@gmail.com>
2019-06-04 16:12:10 +0300

    updated spec to test for config values explicitly

--- spec/lib/nexmo_rails_spec.rb
+++ spec/lib/nexmo_rails_spec.rb
@@ -6,16 +6,23 @@ describe Nexmo do
 
     before do
       described_class.setup do |config|
-        config.api_key        = 'NEXMO_API_KEY',
-        config.api_secret     = 'NEXMO_API_SECRET',
-        config.api_signature  = 'NEXMO_API_SIGNATURE',
-        config.application_id = 'NEXMO_APPLICATION_ID',
-        config.private_key    = 'NEXMO_PRIVATE_KEY'
+        config.api_key           = 'NEXMO_API_KEY'
+        config.api_secret        = 'NEXMO_API_SECRET'
+        config.signature_secret  = 'NEXMO_API_SIGNATURE'
+        config.application_id    = 'NEXMO_APPLICATION_ID'
+        config.private_key       = 'NEXMO_PRIVATE_KEY'
       end
     end
 
     it 'sets up the client' do
-      expect(described_class.client).to be_an_instance_of(::Nexmo::Client)
-    end
+      client = described_class.client
+
+      expect(client).to be_an_instance_of(::Nexmo::Client)
+      expect(client.api_key).to eq('NEXMO_API_KEY')
+      expect(client.api_secret).to eq('NEXMO_API_SECRET')
+      expect(client.signature_secret).to eq('NEXMO_API_SIGNATURE')
+      expect(client.application_id).to eq('NEXMO_APPLICATION_ID')
+      expect(client.private_key).to eq('NEXMO_PRIVATE_KEY')
+     end
   end
 end
