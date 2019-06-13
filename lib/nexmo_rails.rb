f6ef0de99380a883a2bc819b0d406048736d8ca4
benhayehudi <bengreenberg@gmail.com>
2019-06-13 10:25:42 +0300

    added support to read a string for private key if thats what was provided

--- lib/nexmo_rails.rb
+++ lib/nexmo_rails.rb
@@ -21,7 +21,7 @@ module Nexmo
         api_secret: config.api_secret,
         signature_secret: config.signature_secret,
         application_id: config.application_id,
-        private_key: File.read(config.private_key)
+        private_key: File.exist?(config.private_key) ? File.read(config.private_key) : config.private_key
       )
     end
   end
